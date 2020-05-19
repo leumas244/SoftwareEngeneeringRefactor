51571772ecb89d1f7fac4faa95ad081cc85e355d
Samuel-PC <schlingel.samy@web.de>
2020-05-19 19:04:35 +0200

    Replacing the Conditional Logic on Price Code with Polymorphism

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -17,4 +17,33 @@ public class Movie {
     public String getTitle (){
         return title;
     };
+
+    public double getRentCosts(int daysRented) {
+        double result = 0;
+        switch (priceCode) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+    public int addFrequentRenterPoints(int daysRented) {
+        int result = 0;
+        // add frequent renter points
+        result++;
+        // add bonus for a two day new release rental
+        if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+            result++;
+        return result;
+    };
 }
