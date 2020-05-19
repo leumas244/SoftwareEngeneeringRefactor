dbce75cbbb8986c0b507cf9900198f1fa2ded61b
Samuel-PC <schlingel.samy@web.de>
2020-05-19 18:32:24 +0200

    Rename Variables

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -40,24 +40,24 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental myRental) {
+        double result = 0;
+        switch (myRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (myRental.getDaysRented() > 2)
+                    result += (myRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += myRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (myRental.getDaysRented() > 3)
+                    result += (myRental.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return result;
     }
 
 }
