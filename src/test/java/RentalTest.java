99b8a22cd1bd539d55ae1d1527abe8e02762737c
Samuel-PC <schlingel.samy@web.de>
2020-05-19 17:35:09 +0200

    add RentalTestClass

--- src/test/java/RentalTest.java
+++ src/test/java/RentalTest.java
@@ -3,12 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RentalTest {
+    Movie TestMovieClass = new Movie("testMovie", 2);
+    Rental RentalTestClass = new Rental(TestMovieClass, 1);
 
     @Test
     void getDaysRented() {
+        assertEquals(1, RentalTestClass.getDaysRented());
     }
 
     @Test
     void getMovie() {
+        assertEquals(TestMovieClass, RentalTestClass.getMovie());
     }
 }
